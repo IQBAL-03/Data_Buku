7082eb3feac6c7464b2f68a695f49b90e72600e5
IQ-2 <muhamadiqbal9871@gmail.com>
2026-01-26 10:33:19 +0700

    Upload Tugas Data Buku

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header>
+      <h1 id="judul">Data Buku</h1>
+      <div className='tambah'>
+        <button className="btn-tambah">+ Tambah</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </header>
+  )
+}
+
+function BukuCard({ judul, penerbit, tahun }) {
+  return (
+    <div className="card">
+      <h1 className="judul1">{judul}</h1>
+      <p><span>Penerbit: </span>{penerbit}</p>
+      <p><span>Tahun Terbit: </span>{tahun}</p>
+      <button className="edit">Edit</button>
+      <button className="hapus">Hapus</button>
+    </div>
+  )
+}
+
+
+function App() {
+  const buku = [
+    { judul: "Neon Genesis Evangelion", penerbit: "M&C", tahun: "1995" },
+    { judul: "Look Back", penerbit: "M&C", tahun: "2022" },
+    { judul: "Goodbye, Eri", penerbit: "M&C", tahun: "2022" }
+  ];
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <div className="wrap">
+          <div className="container">
+            {buku.map(buku => (
+              <BukuCard
+                judul={buku.judul}
+                penerbit={buku.penerbit}
+                tahun={buku.tahun}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
