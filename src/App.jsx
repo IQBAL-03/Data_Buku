9d9c7e79a49d27421eb7c481f2c4e31e49220663
IQ-2 <muhamadiqbal9871@gmail.com>
2026-02-02 09:38:21 +0700

    Perbaiki kode App.jsx untuk konsistensi kode dan peningkatan format.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import './App.css'
+import "./App.css";
 
 function Header() {
   return (
     <header>
       <h1 id="judul">Data Buku</h1>
-      <div className='tambah'>
+      <div className="tambah">
         <button className="btn-tambah">+ Tambah</button>
       </div>
     </header>
-  )
+  );
 }
 
 function BukuCard({ judul, penerbit, tahun }) {
   return (
     <div className="card">
       <h1 className="judul1">{judul}</h1>
-      <p><span>Penerbit: </span>{penerbit}</p>
-      <p><span>Tahun Terbit: </span>{tahun}</p>
+      <p>
+        <span>Penerbit: </span>
+        {penerbit}
+      </p>
+      <p>
+        <span>Tahun Terbit: </span>
+        {tahun}
+      </p>
       <button className="edit">Edit</button>
       <button className="hapus">Hapus</button>
     </div>
-  )
+  );
 }
-
 
 function App() {
   const buku = [
     { judul: "Neon Genesis Evangelion", penerbit: "M&C", tahun: "1995" },
     { judul: "Look Back", penerbit: "M&C", tahun: "2022" },
-    { judul: "Goodbye, Eri", penerbit: "M&C", tahun: "2022" }
+    { judul: "Goodbye, Eri", penerbit: "M&C", tahun: "2022" },
+    { judul: "Detektive Conan", penerbit: "M&C", tahun: "1994" },
+    { judul: "Dorabase", penerbit: "M&C", tahun: "2000" },
+    { judul: "The Doraemons", penerbit: "M&C", tahun: "1991" },
   ];
   return (
     <div className="App">
@@ -36,8 +44,9 @@ function App() {
       <main>
         <div className="wrap">
           <div className="container">
-            {buku.map(buku => (
+            {buku.map((buku, idx) => (
               <BukuCard
+                key={idx}
                 judul={buku.judul}
                 penerbit={buku.penerbit}
                 tahun={buku.tahun}
@@ -47,7 +56,7 @@ function App() {
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
